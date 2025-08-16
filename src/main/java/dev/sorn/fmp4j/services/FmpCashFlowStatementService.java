2ce759a5f955728e986ac5da49a1d766f210a857
Sorn <maksumic@protonmail.com>
2025-08-16 18:42:09 +0400

    #7 Create CashFlowStatement(Service)

---
+++ src/main/java/dev/sorn/fmp4j/services/FmpCashFlowStatementService.java
@@ -0,0 +1,31 @@
+package dev.sorn.fmp4j.services;
+
+import dev.sorn.fmp4j.cfg.FmpConfig;
+import dev.sorn.fmp4j.http.FmpHttpClient;
+import dev.sorn.fmp4j.models.FmpCashFlowStatement;
+import java.util.Set;
+import static dev.sorn.fmp4j.json.FmpJsonUtils.typeRef;
+
+public class FmpCashFlowStatementService extends FmpService<FmpCashFlowStatement[]> {
+    protected FmpCashFlowStatementService(
+        FmpConfig cfg,
+        FmpHttpClient http
+    ) {
+        super(cfg, http, typeRef(FmpCashFlowStatement[].class));
+    }
+
+    @Override
+    protected String relativeUrl() {
+        return "/cash-flow-statement";
+    }
+
+    @Override
+    protected Set<String> requiredParams() {
+        return Set.of("symbol");
+    }
+
+    @Override
+    protected Set<String> optionalParams() {
+        return Set.of("period", "limit");
+    }
+}
---
+++ src/test/java/dev/sorn/fmp4j/models/FmpCashFlowStatementTest.java
@@ -0,0 +1,22 @@
+package dev.sorn.fmp4j.models;
+
+import dev.sorn.fmp4j.CashFlowStatementTestData;
+import java.io.IOException;
+import org.junit.jupiter.api.Test;
+import static dev.sorn.fmp4j.TestUtils.deserialize;
+import static dev.sorn.fmp4j.TestUtils.serialize;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FmpCashFlowStatementTest implements CashFlowStatementTestData {
+    @Test
+    void is_serializable() throws IOException, ClassNotFoundException {
+        // given
+        var before = anAnnualCashFlowStatement();
+
+        // when
+        var after = (FmpCashFlowStatement) deserialize(serialize(before));
+
+        // then
+        assertEquals(before, after);
+    }
+}
