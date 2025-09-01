e43611c31b731f31d544f3a803ec7797ff4f223f
siddhant <siddhskore@gmail.com>
2025-09-01 20:10:06 +0530

    #14 Fix: set serialVersionUID

--- src/main/java/dev/sorn/fmp4j/models/FmpSearchByCik.java
+++ src/main/java/dev/sorn/fmp4j/models/FmpSearchByCik.java
@@ -7,5 +7,5 @@ public record FmpSearchByCik(
         FmpSymbol symbol, String companyName, String cik, String exchangeFullName, String exchange, String currency)
         implements FmpModel {
     @Serial
-    private static final long serialVersionUID = 2L;
+    private static final long serialVersionUID = 1L;
 }
