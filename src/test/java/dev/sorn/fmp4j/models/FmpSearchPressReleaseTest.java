afa7532c0090b5cebe80345180a5ab59d0ae30b9
Matej Roch <matej.roch4@gmail.com>
2025-11-05 08:36:40 +0100

    #29 implemented Search Press Releases endpoint

---
+++ src/test/java/dev/sorn/fmp4j/models/FmpSearchPressReleaseTest.java
@@ -0,0 +1,34 @@
+package dev.sorn.fmp4j.models;
+
+import static dev.sorn.fmp4j.TestUtils.deserialize;
+import static dev.sorn.fmp4j.TestUtils.serialize;
+import static dev.sorn.fmp4j.TestUtils.verifySerialization;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import dev.sorn.fmp4j.FmpSearchPressReleaseTestData;
+import java.io.IOException;
+import org.junit.jupiter.api.Test;
+
+class FmpSearchPressReleaseTest implements FmpSearchPressReleaseTestData {
+
+    @Test
+    void is_serializable() throws IOException, ClassNotFoundException {
+        // given
+        var before = aPressRelease();
+
+        // when
+        var after = (FmpSearchPressRelease) deserialize(serialize(before));
+
+        // then
+        assertEquals(before, after);
+    }
+
+    @Test
+    void serializes() throws IOException {
+        // given
+        var o = aPressRelease();
+
+        // when // then
+        verifySerialization(o);
+    }
+}
